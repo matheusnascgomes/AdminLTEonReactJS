083ac9f8d2ff38d53c278367c7bb1225925526a6
Matheus Gomes <matheusnascgomes@gmail.com>
2018-07-21 15:47:10 -0300

    first commit

---
+++ src/template/dependencies.js
@@ -0,0 +1,15 @@
+
+import 'node_modules/admin-lte/plugins/jQueryUI/jquery-ui.min'
+import 'node_modules/admin-lte/plugins/fastclick/fastclick'
+import 'node_modules/admin-lte/plugins/slimScroll/jquery.slimscroll.min'
+import 'node_modules/admin-lte/dist/js/app.min'
+
+import 'node_modules/font-awesome/css/font-awesome.min.css'
+import 'node_modules/ionicons/dist/css/ionicons.min.css'
+import 'node_modules/admin-lte/bootstrap/css/bootstrap.min.css'
+
+import 'node_modules/admin-lte/dist/css/AdminLTE.min.css'
+import 'node_modules/admin-lte/dist/css/skins/_all-skins.min.css'
+import 'node_modules/admin-lte/plugins/iCheck/flat/blue.css'
+
+import './custom.css'
