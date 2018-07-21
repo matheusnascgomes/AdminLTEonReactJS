e9a967dc0038fc79e1ca8e336043e998a287a356
Roberto Ribeiro <robertov82008@gmail.com>
2018-07-21 17:48:50 -0300

    fix - resolvendo problemas de dependencias

--- src/template/dependencies.js
+++ src/template/dependencies.js
@@ -1,15 +1,16 @@
 
-import 'node_modules/admin-lte/plugins/jQueryUI/jquery-ui.min'
-import 'node_modules/admin-lte/plugins/fastclick/fastclick'
-import 'node_modules/admin-lte/plugins/slimScroll/jquery.slimscroll.min'
-import 'node_modules/admin-lte/dist/js/app.min'
+import 'jquery/src/jquery'
+import 'admin-lte/plugins/jQueryUI/jquery-ui.min'
+import 'admin-lte/plugins/fastclick/fastclick'
+import 'admin-lte/plugins/slimScroll/jquery.slimscroll'
+import 'admin-lte/dist/js/app.min'
 
-import 'node_modules/font-awesome/css/font-awesome.min.css'
-import 'node_modules/ionicons/dist/css/ionicons.min.css'
-import 'node_modules/admin-lte/bootstrap/css/bootstrap.min.css'
+import 'font-awesome/css/font-awesome.min.css'
+import 'ionicons/dist/css/ionicons.min.css'
+import 'admin-lte/bootstrap/css/bootstrap.min.css'
 
-import 'node_modules/admin-lte/dist/css/AdminLTE.min.css'
-import 'node_modules/admin-lte/dist/css/skins/_all-skins.min.css'
-import 'node_modules/admin-lte/plugins/iCheck/flat/blue.css'
+import 'admin-lte/dist/css/AdminLTE.min.css'
+import 'admin-lte/dist/css/skins/_all-skins.min.css'
+import 'admin-lte/plugins/iCheck/flat/blue.css'
 
 import './custom.css'
